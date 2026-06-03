9e5a3d5dc4f38c544a6db8d5135c12dee8472752
Trusty <ivanbordeira2015@hotmail.com>
2026-06-03 17:47:22 -0300

    Implementacion de las rutinas de atencion

--- TPfinal.X/main.c
+++ TPfinal.X/main.c
@@ -14,8 +14,17 @@
 //Colocar rutinas de atencion aqui
 //Configuracion de puertos e inicio del programa
 
+void __attribute__((interrupt, auto_psv)) _DMA0Interrupt(void) {
+    /* Rutina de atención para la interrupción del DMA */
+    IFS0bits.DMA0IF = 0;
+}
+
+void __attribute__((interrupt, auto_psv)) _T1Interrupt(void) {
+    /* Rutina de atención para la interrupción del Timer 1 */
+    IFS0bits.T1IF = 0;
+}
+
 int main(void) {
-    TRISA()
     InitADC();
     InitDMA();
     Init_Timer1();
